4a97b93d64594d90276cec26c3ddf7381831e1e0
Josh Siva <jtsiva@gmail.com>
2013-11-06 13:35:25 -0800

    separated prototype from adminTools.cpp and removed prototype stuff from adminTools.cpp

--- Makefile
+++ Makefile
@@ -14,13 +14,19 @@ VALGRIND_OPTIONS=-v --leak-check=yes --track-origins=yes
 
 .PHONY: all clean package
 
-all: bin/adminTools
+all: bin/adminTools bin/prototype
 
 bin/adminTools: bin/adminTools.o include/adminTools.h 
 	${CC} ${CFLAGS} -o bin/adminTools bin/adminTools.o -lcurl
-	
+
+bin/prototype: bin/prototype.o include/adminTools.h
+	${CC} ${CLFAGS} -o bin/prototype bin/prototype.o -lcurl
+
 bin/adminTools.o: src/adminTools.cpp include/adminTools.h 
 	${CC} ${CFLAGS} -o bin/adminTools.o -c src/adminTools.cpp
+	
+bin/prototype.o: src/prototype.cpp include/adminTools.h
+	${CC} ${CFLAGS} -o bin/prototype.o -c src/prototype.cpp
 
 package:
 	tar czf NachoNet.tar.gz bin/ include/ src/ Makefile;
--- src/adminTools.cpp
+++ src/adminTools.cpp
@@ -12,72 +12,8 @@ Purpose:		Provides the structure for testing modules, managing nodes, and
 #include <iostream>
 #include <stdlib.h>
 
-//For prototype only (if used in later code it wouldn't be included here)
-#include <curl/curl.h>
-#include <fstream>
-#include <sstream>
 
 const int BACK = -1;
-
-//from http://www.cplusplus.com/forum/unices/45878/
-static size_t data_write(void* buf, size_t size, size_t nmemb, void* userp)
-{
-	if(userp)
-	{
-		std::ostream& os = *static_cast<std::ostream*>(userp);
-		std::streamsize len = size * nmemb;
-		if(os.write(static_cast<char*>(buf), len))
-			return len;
-	}
-
-	return 0;
-}
-
-CURLcode curl_read(const std::string& url, std::ostream& os, long timeout = 30)
-{
-	CURLcode code(CURLE_FAILED_INIT);
-	CURL* curl = curl_easy_init();
-
-	if(curl)
-	{
-		if(CURLE_OK == (code = curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &data_write))
-		&& CURLE_OK == (code = curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L))
-		&& CURLE_OK == (code = curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L))
-		&& CURLE_OK == (code = curl_easy_setopt(curl, CURLOPT_FILE, &os))
-		&& CURLE_OK == (code = curl_easy_setopt(curl, CURLOPT_TIMEOUT, timeout))
-		&& CURLE_OK == (code = curl_easy_setopt(curl, CURLOPT_URL, url.c_str())))
-		{
-			code = curl_easy_perform(curl);
-		}
-		curl_easy_cleanup(curl);
-	}
-	return code;
-}
-
-CURLcode curl_post(const std::string& url)
-{
-	CURLcode code(CURLE_FAILED_INIT);
-	CURL* curl = curl_easy_init();
-	struct curl_slist* headers = NULL;
-	headers = curl_slist_append(headers, "Content-Type: application/json");
-
-	if(curl)
-	{
-		if(CURLE_OK == (code = curl_easy_setopt(curl, CURLOPT_POSTFIELDS,
-				"{\"source\":\"http://192.168.1.11:5984/nachonet\", \"target\":\"nachonet\"}"))
-		&& CURLE_OK == (code = curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers))
-		&& CURLE_OK == (code = curl_easy_setopt(curl, CURLOPT_URL, url.c_str())))
-		{
-			code = curl_easy_perform(curl);
-		}
-		curl_easy_cleanup(curl);
-	}
-
-	curl_slist_free_all(headers);
-
-	return code;
-
-}
 
 int menu(int menuID)
 {
@@ -150,100 +86,11 @@ int menu(int menuID)
 	return selection;
 }
 
-void cleanJSONData(std::string jsonData)
-{
-	std::string word;
-	bool bInArray = false;
-	int objNum = -1;
-
-	for(unsigned int i = 0; i < jsonData.length(); i++)
-	{
-		switch(jsonData[i])
-		{
-			case '{':
-				objNum++;
-				break;
-
-			case '}':
-				objNum--;
-				break;
-
-			case '[':
-				std::cout << std::endl;
-				bInArray = true;
-				break;
-
-			case ']':
-				bInArray = false;
-				break;
-
-			case '"':
-				if(bInArray)
-				{
-					word.push_back('\t');
-				}
-
-				while('"' != jsonData[++i])
-				{
-					word.push_back(jsonData[i]);
-				}
-
-				std::cout << word << " ";
-
-				word.clear();
-				break;
-
-			case ',':
-				if(0 == objNum)
-				{
-					std::cout << std::endl;
-				}
-				else
-				{
-					std::cout << "\t";
-				}
-
-
-				break;
-
-			case ':':
-				++i;
-
-				if('[' == jsonData[i])
-				{
-					--i;
-				}
-				else
-				{
-					std::cout << " ";
-
-					while(',' != jsonData[i] && '}' != jsonData[i])
-					{
-						word.push_back(jsonData[i]);
-						++i;
-					}
-
-					std::cout << word << " ";
-
-					word.clear();
-
-					--i;
-				}
-				break;
-
-		}
-
-	}
-
-}
 
 int main()
 {
 	int menuID = AdminTools::MAIN;
 	int menuSelection;
-	curl_global_init(CURL_GLOBAL_ALL); //same location as other curl code
-	std::string json;
-	std::ostringstream oss;
 
 	while (AdminTools::EXIT != menuSelection || AdminTools::MAIN != menuID)
 	{
@@ -262,27 +109,6 @@ int main()
 						break;
 
 					case AdminTools::START:
-						//FOR PROTOTYPE DEMO ONLY!!!
-						//replicate database and print
-
-						if(CURLE_OK == curl_post("http://192.168.1.10:5984/_replicate"))
-						{
-
-
-						}
-
-
-						//from same place as other curl code
-						if(CURLE_OK == curl_read("http://192.168.1.10:5984/nachonet/node_2", oss))
-						{
-							// Web page successfully written to string
-							json = oss.str();
-							oss.str("");
-						}
-
-						std::cout << "\n\n---------------------------------------------------------\n";
-						cleanJSONData(json);
-						std::cout << "\n---------------------------------------------------------\n\n\n";
 
 						break;
 
@@ -290,18 +116,6 @@ int main()
 						break;
 
 					case AdminTools::LIST_DEVICES:
-						//FOR PROTOTYPE DEMO ONLY!!!
-						//pull data from database and print
-						if(CURLE_OK == curl_read("http://192.168.1.10:5984/nachonet/locations", oss))
-						{
-							// Web page successfully written to string
-							json = oss.str();
-							oss.str("");
-						}
-
-						std::cout << "\n\n---------------------------------------------------------\n";
-						cleanJSONData(json);
-						std::cout << "\n---------------------------------------------------------\n\n\n";
 
 						break;
 

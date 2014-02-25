2fa097d1213781b261dbfe6f49f8f6054439f9a3
Josh Siva <jtsiva@gmail.com>
2014-02-24 16:59:07 -0800

    created second jsonParser object that deals with translating to a JSON object. The lexer part looks pretty good so work has begun on the parser

--- include/util/json.h
+++ include/util/json.h
@@ -8,44 +8,21 @@ Purpose:		Defines the interface for the json object which is used to parse
 *******************************************************************************/
 
 #pragma once
+#include "jsonParser.h"
 #include <map>
-
-union jsonVal
-{
-	int intVal;
-	std::string strVal;
-	float floatVal;
-	JSON object;
-	std::vector<jsonVal> array;
-	bool boolVal;
-} jsonVal;
 
 class JSON
 {
 	public:
-    JSON (std::string rawJSON);
+    JSON ();
     ~JSON ();
-    void init (std::string rawJSON);
-    bool getBool (std::string key);
-    int getInt (std::string key);
-    float getFloat (std::string key);
-    std::string getString (std::string key);
-    std::vector<jsonVal> getVector (std::string key);
+    jsonVal getData(std::string key);
     void setValue (std::string key, jsonVal value);
 
     std::string writeJSON ();
 
-    enum Type {NULL, INT, STRING, BOOL, FLOAT, ARRAY, OBJECT};
-
-    const char L_CURL = '{';
-    const char R_CURL = '}';
-    const char L_BRKT = '[';
-    const char R_BRKT = ']';
-    const char COMMA = ',';
-    const char COLON = ':';
-
   private:
-    bool validJSON;
+
     std::map<std::string, jsonVal> keyVal;
 
 };

b2dba7586de66114288fe6f3809129666adfce6e
Josh Siva <jtsiva@gmail.com>
2014-03-31 10:36:47 -0700

    added missing files in project

---
+++ src/nacho/NachoNet.cpp
@@ -0,0 +1,489 @@
+/*******************************************************************************
+File:				NachoNet.cpp
+Author: 		Josh Siva
+Date:				3/19/14
+Project:		NachoNet
+Purpose:		Implements the behavior of the NachoNet object which is the object
+						that integrates all of the pieces of NachoNet (including admin
+						functionality)
+*******************************************************************************/
+
+#include "../../include/nacho/NachoNet.h"
+
+const std::string NachoNet::DIST_CONFIG_FILE = "dist.conf";
+
+/*******************************************************************************
+ * Constructor:	NachoNet
+ *
+ * Description:	Initializes the debug and verbose variables. Sets up the modules
+ * 							and kicks of the listener who will listen for things to do as
+ * 							soon as the data exchange module is initialized
+ *
+ * Parameters:	None
+ *
+ * Returned:		None
+ ******************************************************************************/
+NachoNet::NachoNet (bool debug, bool verbose)
+{
+	Config distConfig (DIST_CONFIG_FILE);
+	this->debug = debug;
+	this->verbose = verbose;
+
+	if (isVerbose ())
+	{
+		std::cout << "Initializing modules\n";
+	}
+
+	pMap = new MapOnTheCouch;
+	pMap->load ();
+	pDataCollect = new stdCollect (stdCollect::DEFAULT_IFACE, debug);
+	pDistMeasure = new pathLoss (debug, &distConfig);
+	pDataEx = NULL;
+	pLocalization = new localization (debug);
+	pWorker = NULL;
+
+	active = true;
+	pListener = new std::thread (&NachoNet::listener, this);
+
+	if (isVerbose ())
+	{
+		std::cout << "Message listener started\n";
+	}
+}
+
+/*******************************************************************************
+ * Destroyer!:	~NachoNet
+ *
+ * Description:	Delete all modules and kill all threads. This includes shutting
+ * 							down NachoNet if it's running.
+ *
+ * Parameters:	None
+ *
+ * Returned:		None
+ ******************************************************************************/
+NachoNet::~NachoNet ()
+{
+	if (NULL != pWorker)
+	{
+		if (isVerbose ())
+		{
+			std::cout << "Stopping NachoNet\n";
+		}
+
+		pDataEx->setIsAlive (false);
+
+		pWorker->join ();
+		delete pWorker;
+
+		pWorker = NULL;
+	}
+
+	if (isVerbose ())
+	{
+		std::cout << "Stopping message listener\n";
+	}
+
+	active = false;
+	pListener->join ();
+	delete pListener;
+
+
+	if (isVerbose ())
+	{
+		std::cout << "Deleting modules\n";
+	}
+
+	delete pMap;
+	delete pDataCollect;
+	delete pDistMeasure;
+
+	if (NULL != pDataEx)
+	{
+		delete pDataEx;
+	}
+
+	delete pLocalization;
+
+	if (isVerbose ())
+	{
+		std::cout << "Adios!\n";
+	}
+}
+
+/*******************************************************************************
+ * Method:			stop
+ *
+ * Description: Stop the worker thread (which carries out all data collection
+ * 							and calculations) and tell other nodes to the same.
+ *
+ * Parameters:	None
+ *
+ * Returned:		None
+ ******************************************************************************/
+void NachoNet::stop ()
+{
+	Message message;
+
+	if (!pDataEx->alive ())
+	{
+		std::cout << "NachoNet is already stopped... Now doing less than nothing.\n";
+	}
+	else if (NULL == pDataEx)
+	{
+		std::cout << "This node has not yet been added to NachoNet.\n";
+	}
+	else
+	{
+		pDataEx->setIsAlive (false);
+
+		pWorker->join ();
+		delete pWorker;
+
+		pWorker = NULL;
+
+		message.msg = dataEx::STOP;
+
+		for (int id : pDataEx->getNodeIDs ())
+		{
+			message.dest.push_back (id);
+		}
+
+		pDataEx->ping (message);
+
+		if (isVerbose ())
+		{
+			std::cout << "No more nachos :'(\n";
+		}
+	}
+}
+
+/*******************************************************************************
+ * Method:			start
+ *
+ * Description: Start the worker thread (which carries out all data collection
+ * 							and calculations) and send a message to other nodes in
+ * 							NachoNet to do the same.
+ *
+ * Parameters:	None
+ *
+ * Returned:		None
+ ******************************************************************************/
+void NachoNet::start ()
+{
+	Message message;
+	if (pDataEx->alive ())
+	{
+		std::cout << "NachoNet is already running\n";
+	}
+	else if (NULL == pDataEx)
+	{
+		std::cout << "This node has not yet been added to NachoNet.\n";
+	}
+	else
+	{
+		pDataEx->setIsAlive (true);
+
+		pWorker = new std::thread (&NachoNet::worker, this);
+
+		message.msg = dataEx::START;
+
+		for (int id : pDataEx->getNodeIDs ())
+		{
+			message.dest.push_back (id);
+		}
+
+		pDataEx->ping (message);
+
+		if (isVerbose ())
+		{
+			std::cout << "Nachos for everyone! :D\n";
+		}
+	}
+
+}
+
+/*******************************************************************************
+ * Method:			worker
+ *
+ * Description: Gathers signal strengths for devices in the area, calculates
+ * 							distances from these measurements then shares these measurements
+ * 							with the data exchange module. Also pulls data out of the data
+ * 							exchange module to localize a device. This data is then pushed
+ * 							back into the data exchange module. (runs as a thread)
+ *
+ * Parameters:	None
+ *
+ * Returned:		None
+ ******************************************************************************/
+void NachoNet::worker ()
+{
+	std::vector<ssMeasurement> signalStrengths;
+	std::vector<distMeasurement> distances;
+	std::vector<refMeasurement> referenceDistances;
+	location devLocation;
+
+	while (pDataEx->alive ())
+	{
+		//get updated node data
+		pDataEx->pullUpdates (dataExOnTheCouch::NODES);
+
+		//collect data from network
+		pDataCollect->readFromNetwork ();
+		signalStrengths = pDataCollect->getSS ();
+
+		//calculate distances from signal strengths
+		for (auto & ss : signalStrengths)
+		{
+			distances.push_back (pDistMeasure->measure (ss));
+		}
+
+		//update measurement information in data exchange
+		for (auto & dist : distances)
+		{
+			pDataEx->updateDevMeasurement (dist);
+		}
+
+		//share new data with the rest of NachoNet
+		pDataEx->pushUpdates (dataExOnTheCouch::NODES);
+
+		//get updated device data
+		pDataEx->pullUpdates (dataExOnTheCouch::DEVICES);
+
+		//get reference measurements for a valid device
+		referenceDistances = pDataEx->getMeasurements (pDataEx->getDevForUpdate ());
+
+		//we must have exactly 3 measurements to trilaterate
+		if (REQUIRED_MEASUREMENTS == referenceDistances.size ())
+		{
+			//trilateration
+			devLocation = pLocalization->localize (referenceDistances[0],
+																						 referenceDistances[1],
+																						 referenceDistances[2]);
+
+			//update device location in data exchange
+			pDataEx->updateDevLocation (devLocation.theID.strID, devLocation);
+		}
+
+		//share updated location with the rest of NachoNet
+		pDataEx->pushUpdates (dataExOnTheCouch::DEVICES);
+
+		signalStrengths.clear ();
+		distances.clear ();
+		referenceDistances.clear ();
+	}
+}
+
+/*******************************************************************************
+ * Method:			listener
+ *
+ * Description:	Handles all checking of messages for the data exchange module
+ * 							and listens for a command to start or stop running. Kicks off
+ * 							or kills the worker thread accordingly. (runs as a thread)
+ *
+ * Parameters:	None
+ *
+ * Returned:		None
+ ******************************************************************************/
+void NachoNet::listener ()
+{
+	bool stateChange = false;
+
+	while (active)
+	{
+		if (NULL != pDataEx)
+		{
+			pDataEx->checkMessages ();
+
+			stateChange = stateChange ^ pDataEx->alive ();
+
+			if (stateChange && pDataEx->alive ())
+			{
+				pDataEx->setIsAlive (true);
+
+				pWorker = new std::thread (&NachoNet::worker, this);
+			}
+			else if (stateChange && !pDataEx->alive ())
+			{
+				stateChange = false;
+
+				pWorker->join ();
+				delete pWorker;
+
+				pWorker = NULL;
+
+			}
+		}
+	}
+}
+
+/*******************************************************************************
+ * Method:			listDevices
+ *
+ * Description:	Prints a nice table of node IDs, device IDs and their locations
+ *
+ * Parameters:	None
+ *
+ * Returned:		None
+ ******************************************************************************/
+void NachoNet::listDevices ()
+{
+	std::vector<location> nodeLocations;
+	std::vector<location> devLocations;
+
+	if (NULL == pDataEx)
+	{
+		std::cout << "This node has not been added to NachoNet.\n";
+	}
+	else
+	{
+		nodeLocations = pDataEx->getNodeLocations ();
+		devLocations = pDataEx->getDeviceLocations ();
+
+		std::cout << "                     LOCATIONS\n"
+							<< "                     ---------\n"
+							<< "Nodes:\n";
+
+		for (auto & nodeLoc : nodeLocations)
+		{
+			std::cout << nodeLoc.theID.intID << "   (" << nodeLoc.x << ", "
+																								 << nodeLoc.y << ")\n";
+		}
+		std::cout << "\n--------------------------------------------------------\n";
+		std::cout << "Devices:\n";
+
+		for (auto & devLoc : devLocations)
+		{
+			std::cout << devLoc.theID.strID << "   ";
+
+			if (0.0f == devLoc.x && 0.0f == devLoc.y)
+			{
+				std::cout << "(?,?)\n";
+			}
+			else
+			{
+				std::cout << "(" << devLoc.x << ", " << devLoc.y << ")\n";
+			}
+		}
+
+	}
+}
+
+/*******************************************************************************
+ * Method:			addNode
+ *
+ * Description:	Add this node to NachoNet provided it has not already been added
+ *
+ * Parameters:	None
+ *
+ * Returned:		None
+ ******************************************************************************/
+void NachoNet::addNode ()
+{
+	if (NULL != pDataEx)
+	{
+		std::cout << "This node has already been added.\n";
+	}
+	else
+	{
+		if (isVerbose ())
+		{
+			std::cout << "Adding node to NachoNet...\n";
+		}
+
+		pDataEx = new dataExOnTheCouch ();
+
+
+		if (isVerbose ())
+		{
+			std::cout << "Node added! Be sure to update the node's location under the"
+								<< " management menu.\n";
+		}
+	}
+}
+
+/*******************************************************************************
+ * Method:			dropNode
+ *
+ * Description:	Remove a node from NachoNet if it exists in NachoNet.
+ *
+ * Parameters:	None
+ *
+ * Returned:		None
+ ******************************************************************************/
+void NachoNet::dropNode ()
+{
+	if (NULL == pDataEx)
+	{
+		std::cout << "This node has not been added.\n";
+	}
+	else
+	{
+		if (isVerbose ())
+		{
+			std::cout << "Removing node from NachoNet...\n";
+		}
+
+		delete pDataEx;
+		pDataEx = NULL;
+
+
+		if (isVerbose ())
+		{
+			std::cout << "Node removed.\n";
+		}
+	}
+}
+
+/*******************************************************************************
+ * Method:			updateMap
+ *
+ * Description:	Get user input to update the map of the room. If the data is
+ * 							valid then it is saved.
+ *
+ * Parameters:	None
+ *
+ * Returned:		None
+ ******************************************************************************/
+void NachoNet::updateMap ()
+{
+	pMap->load ();
+
+	if (!pMap->getUserInput ())
+	{
+		std::cout << "Failed to set map.\n";
+	}
+	else
+	{
+		pMap->save ();
+	}
+}
+
+/*******************************************************************************
+ * Method:			isDebug
+ *
+ * Description: Returns whether debug mode is on
+ *
+ * Parameters:	None
+ *
+ * Returned:		bool
+ ******************************************************************************/
+bool NachoNet::isDebug ()
+{
+	return this->debug;
+}
+
+/*******************************************************************************
+ * Method:			isVerbose
+ *
+ * Description: Returns whether verbose mode is on
+ *
+ * Parameters:	None
+ *
+ * Returned:		bool
+ ******************************************************************************/
+bool NachoNet::isVerbose ()
+{
+	return this->verbose;
+}
+
+
---
+++ src/tools/main.cpp
@@ -0,0 +1,310 @@
+/*******************************************************************************
+File:
+Author: 		Josh Siva
+Date:
+Class:			CS460
+Assignment:
+Purpose:
+*******************************************************************************/
+
+#include "../../include/nacho/NachoNet.h"
+#include "../../include/tools/DataCollectAdmin.h"
+#include "../../include/tools/DistMeasureAdmin.h"
+#include "../../include/tools/DataExAdmin.h"
+#include "../../include/tools/LocalizationAdmin.h"
+#include <string.h>
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+
+const int BACK = -1;
+
+typedef enum Menus { MAIN = 0,
+									 TEST,
+									 MANAGE
+								 } Menus;
+
+typedef enum MainMenu { EXIT = -1,
+												TEST_MENU,
+												MANAGE_MENU,
+												START,
+												STOP,
+												LIST_DEVICES,
+												UPDATE_MAP
+											} MainMenu;
+
+typedef enum TestMenu { DATA_COLLECTION = 0,
+												DIST_MEASUREMENT,
+												DATA_EXCHANGE,
+												LOCALIZATION
+											} TestMenu;
+
+typedef enum ManageMenu { ADD_NODE = 0,
+													DROP_NODE,
+													SET_DIST_EQN,
+													CONFIGURE_DIST_EQN,
+													SET_NODE_LOC
+											} ManageMenu;
+
+/*******************************************************************************
+ * Function:
+ *
+ * Description:
+ *
+ * Parameters:
+ *
+ * Returned:
+ ******************************************************************************/
+void clearScreen ()
+{
+	std::cout << std::string (100, '\n');
+}
+
+/*******************************************************************************
+ * Function:
+ *
+ * Description:
+ *
+ * Parameters:
+ *
+ * Returned:
+ ******************************************************************************/
+int menuSelection (int menuID)
+{
+	int selection;
+
+	switch(menuID)
+	{
+		case MAIN:
+
+			std::cout << "Main Menu\n";
+			std::cout << "---------\n\n";
+
+			do
+			{
+				std::cout << "-1. Exit\n";
+				std::cout << " 0. Test\n";
+				std::cout << " 1. Node Management\n";
+				std::cout << " 2. Start\n";
+				std::cout << " 3. Stop\n";
+				std::cout << " 4. List Devices\n";
+				std::cout << " 5. Update Map\n\n";
+
+				std::cout << "Enter Selection: ";
+				std::cin >> selection;
+			} while (EXIT > selection || UPDATE_MAP < selection);
+
+			break;
+
+		case TEST:
+
+			std::cout << "Test Menu\n";
+			std::cout << "---------\n\n";
+
+			do
+			{
+				std::cout << "-1. Back\n";
+				std::cout << " 0. Data Collection\n";
+				std::cout << " 1. Distance Measurement\n";
+				std::cout << " 2. Data Exchange\n";
+				std::cout << " 3. Localization\n\n";
+
+				std::cout << "Enter Selection: ";
+				std::cin >> selection;
+			}while (BACK > selection || LOCALIZATION < selection);
+
+			break;
+
+		case MANAGE:
+
+			std::cout << "Manage Menu\n";
+			std::cout << "-----------\n\n";
+
+			do
+			{
+				std::cout << "-1. Back\n";
+				std::cout << " 0. Add Node\n";
+				std::cout << " 1. Drop Node\n";
+				std::cout << " 2. Set Distance Equation\n";
+				std::cout << " 3. Configure Distance Measure\n";
+				std::cout << " 4. Set Node Location\n\n";
+
+				std::cout << "Enter Selection: ";
+				std::cin >> selection;
+			}while (BACK > selection || SET_NODE_LOC < selection);
+
+			break;
+	}
+
+
+	return selection;
+}
+
+/*******************************************************************************
+ * Function:
+ *
+ * Description:
+ *
+ * Parameters:
+ *
+ * Returned:
+ ******************************************************************************/
+int main (int argc, char** argv)
+{
+	int menuID = MAIN;
+	NachoNet *pMyNachos;
+	Admin *pMyAdmin;
+	int optionChar;
+	bool debug = false, verbose = false, done = false;
+
+	while ((optionChar = getopt (argc, argv, "dv")) != -1)
+	{
+		switch (optionChar)
+		{
+			case 'd':
+				debug = true;
+				break;
+			case 'v':
+			 verbose = true;
+			 break;
+			case '?':
+			 std::cout << "\nHELP\n";
+			 std::cout << "Due to the libpcap integration, this must be run with "
+								 <<	"sudo\n";
+			 std::cout << "Options\n";
+			 std::cout << "d - turn debug mode on\n"
+					       << "v - turn verbose mode on\n";
+
+			 done = true;
+			 break;
+		 }
+	}
+
+	pMyNachos = new NachoNet (debug, verbose);
+
+	while (!done || MAIN != menuID)
+	{
+		switch(menuID)
+		{
+			case MAIN:
+
+				switch (menuSelection (menuID))
+				{
+					case TEST_MENU:
+						menuID = TEST;
+						break;
+
+					case MANAGE_MENU:
+						menuID = MANAGE;
+						break;
+
+					case START:
+						pMyNachos->start ();
+						break;
+
+					case STOP:
+						pMyNachos->stop ();
+						break;
+
+					case LIST_DEVICES:
+						pMyNachos->listDevices ();
+						break;
+
+					case UPDATE_MAP:
+						pMyNachos->updateMap ();
+						break;
+
+					case EXIT:
+						done = true;
+						break;
+				}
+
+				break;
+
+			case TEST:
+
+				switch (menuSelection (menuID))
+				{
+					case DATA_COLLECTION:
+						pMyAdmin = new DataCollectAdmin (pMyNachos);
+						break;
+
+					case DIST_MEASUREMENT:
+						pMyAdmin = new DistMeasureAdmin (pMyNachos);
+						break;
+
+					case DATA_EXCHANGE:
+						pMyAdmin = new DataExAdmin (pMyNachos);
+						break;
+
+					case LOCALIZATION:
+						pMyAdmin = new LocalizationAdmin (pMyNachos);
+						break;
+
+					case BACK:
+						menuID = MAIN;
+						break;
+				}
+
+				if (MAIN != menuID)
+				{
+					pMyAdmin->test ();
+					delete pMyAdmin;
+					pMyAdmin = NULL;
+				}
+
+				break;
+
+			case MANAGE:
+
+				switch (menuSelection (menuID))
+				{
+					case ADD_NODE:
+						pMyNachos->addNode ();
+						break;
+
+					case DROP_NODE:
+						pMyNachos->dropNode ();
+						break;
+
+					case SET_DIST_EQN:
+						pMyAdmin = new DistMeasureAdmin (pMyNachos);
+						pMyAdmin->options ();
+						break;
+
+					case CONFIGURE_DIST_EQN:
+						pMyAdmin = new DistMeasureAdmin (pMyNachos);
+						pMyAdmin->configure ();
+						break;
+
+					case SET_NODE_LOC:
+						pMyAdmin = new DataExAdmin (pMyNachos);
+						pMyAdmin->configure ();
+						break;
+
+					case BACK:
+						menuID = MAIN;
+						break;
+
+				}
+
+				if (NULL != pMyAdmin)
+				{
+					delete pMyAdmin;
+					pMyAdmin = NULL;
+				}
+				break;
+		}
+
+		//std::cout << "==========================================\n";
+		//std::cout << "\n\n\n\n\n\n";
+		clearScreen ();
+
+	}
+
+	delete pMyNachos;
+
+	return 0;
+}
+
